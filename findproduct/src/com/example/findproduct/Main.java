a822cc9b6e766b6cfc04f6605e71bae194576069
skullacy <skullacy@gmail.com>
2013-09-19 16:53:43 +0900

    Signed-off-by: skullacy <skullacy@gmail.com>

--- findproduct/src/com/example/findproduct/Main.java
+++ findproduct/src/com/example/findproduct/Main.java
@@ -1,8 +1,15 @@
 package com.example.findproduct;
 
+import com.example.findproduct.Main;
+import com.example.findproduct.R;
+import com.example.findproduct.smap;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends Activity {
 
@@ -10,6 +17,28 @@ public class Main extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		//습득한 물건 등록버튼 클릭이벤트 등록
+		Button regfindBtn = (Button) findViewById(R.id.regfind);
+		regfindBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(Main.this, smap.class);
+				intent.putExtra("type", "find");
+				startActivity(intent);
+			}
+		});
+		
+		//분실한 물건 등록버튼 클릭이벤트 등록
+		Button reglostBtn = (Button) findViewById(R.id.reglost);
+		reglostBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(Main.this, smap.class);
+				intent.putExtra("type", "lost");
+				startActivity(intent);
+			}
+		});
 	}
 
 	@Override
